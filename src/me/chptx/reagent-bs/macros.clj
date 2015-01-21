40a7e98ee200554abcbab46d3fbc1e98bb4862ae
chptxy <chptxy@gmail.com>
2015-01-21 22:13:54 +0800

    add full list of BootStrap components

--- src/me/chptx/reagent-bs/macros.clj
+++ src/me/chptx/reagent-bs/macros.clj
@@ -1,10 +1,54 @@
 (ns me.chptx.reagent-bs.macros)
 
 (def bs-classes
-  '[Button
+  '[Accordion
+    Affix
+    AffixMixin
+    Alert
+    BootstrapMixin
+    Badge
+    Button
+    ButtonGroup
+    ButtonToolbar
+    Carousel
+    CarouselItem
+    Col
+    CollapsableMixin
+    DropdownButton
+    DropdownMenu
+    DropdownStateMixin
+    FadeMixin
+    Glyphicon
+    Grid
     Input
-    Pannel
-    Glyphicon])
+    Interpolate
+    Jumbotron
+    Label
+    ListGroup
+    ListGroupItem
+    MenuItem
+    Modal
+    Nav
+    Navbar
+    NavItem
+    ModalTrigger
+    OverlayTrigger
+    OverlayMixin
+    PageHeader
+    Panel
+    PanelGroup
+    PageItem
+    Pager
+    Popover
+    ProgressBar
+    Row
+    SplitButton
+    SubNav
+    TabbedArea
+    Table
+    TabPane
+    Tooltip
+    Well])
 
 (defn bsreact-import [tname]
   `(def ~tname (aget js/ReactBootstrap ~(name tname))))
